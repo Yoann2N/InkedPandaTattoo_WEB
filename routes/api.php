528a3b6a3ec86eab7148aa7edd5b0cdd0fc3ec8b
yoann <yoanngervais.yg13750@gmail.com>
2025-12-15 15:29:18 +0100

    api

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ArtisteController;
+use App\Http\Controllers\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('posts', ArtisteController::class);
+
+
+Route::apiResource('posts', PostController::class);
