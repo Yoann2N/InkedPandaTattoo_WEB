29181080bb1165123889cea12e2b6d42bc004bec
yoann <yoanngervais.yg13750@gmail.com>
2026-04-27 12:54:59 +0200

    API login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ArtisteController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,5 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('artistes', ArtisteController::class);
+
+Route::post('/login', [AuthController::class, 'login']);
