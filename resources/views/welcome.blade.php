928ec7893d3b170636da8530c09c2e47e8bc5b66
yoann <yoanngervais.yg13750@gmail.com>
2026-05-22 11:40:23 +0200

    référencement

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Studio à l'isle sur la sorgue Spécialisé dans le tatouage, le piercing, le nailhart et le headspa. >
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'InkedPandaStudio') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
