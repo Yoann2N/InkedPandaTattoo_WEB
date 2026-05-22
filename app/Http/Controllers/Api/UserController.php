9295fc9b3e9344f9ef803bf91960d03f9c819cd5
Philippe Kurz <philippe.kurz@gmail.com>
2026-05-22 14:48:46 +0200

    Excepter password dazns la maj de user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -39,7 +39,8 @@ class UserController extends Controller
         if (!$user) {
             return response()->json(['message' => 'User not found'], 404);
         }
-        $user->update($request->all());
+        $data = $request->except(['password', 'id']); // on exclut password et id    
+        $user->update($data);
         return response()->json($user);
     }
 
