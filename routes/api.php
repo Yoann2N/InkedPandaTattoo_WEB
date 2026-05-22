d77a7bec9532b68fd39d3fff9692ca0e8b342ec5
yoann <yoanngervais.yg13750@gmail.com>
2026-05-22 14:25:51 +0200

    ok

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ArtisteController;
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\api\UserController;
+use App\Http\Controllers\Api\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
